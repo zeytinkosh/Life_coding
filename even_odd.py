15838791e32d7ce0d248db277f0cfa8ffaef8b82
Zeytin <163149798+zeytinkosh@users.noreply.github.com>
2024-03-20 19:09:29 +0200

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+user_input = int(input('Введите число: '))
+
+if user_input % 2 == 0:
+    print(f'ВВеденное число {user_input} - четное')
+else:
+    print(f'Введенное число {user_input} - нечетное')
