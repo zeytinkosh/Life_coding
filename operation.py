5428af836986403d5f679c5399447c8bdfcafdf2
Zeytin <163149798+zeytinkosh@users.noreply.github.com>
2024-03-24 20:39:36 +0200

    Create operation.py

---
+++ operation.py
@@ -0,0 +1,19 @@
+num1 = input('Введите первое число: ')
+num2 = input('Введите второе число: ')
+
+# Сумма двух целых чисел
+sum_ = int(num1) + int(num2)
+
+# Разница двух целых чисел
+sub = int(num1) - int(num2)
+
+# Произведение двух чисел
+mul = float(num1) * float(num2)
+
+# Частное двух чисел
+div = float(num1) / float(num2)
+
+print(f'Сумма чисел {num1} и {num2} равна {sum_}')
+print(f'Разница чисел {num1} и {num2} равна {sub}')
+print(f'Произведение чисел {num1} и {num2} равна {mul}')
+print(f'Частное чисел {num1} и {num2} равна {div}')
