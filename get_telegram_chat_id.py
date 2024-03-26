12786d4738b196a30e81c9d97bbdfb884ecc12e2
Zeytin <163149798+zeytinkosh@users.noreply.github.com>
2024-03-26 21:01:22 +0200

    Create get_telegram_chat_id.py

---
+++ get_telegram_chat_id.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def get_telegram_chat_id():
+    TOKEN = "token"
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+
+    response = requests.get(url).json()
+
+    if 'result' in response and response['result']:
+        chat_id = response['result'][0]['message']['chat']['id']
+        return chat_id
+    else:
+        print("Невозможно получить chat ID.")
+        return None
+
+
+print(get_telegram_chat_id())
+
+
+
+#def send_telegram_message():
+#
+#    api_key = "6576223609:AAFyKRhiBQzQvcoeL0-h-FbjBG8XFdOhe-s"
+#
+#    url = f'https://api.telegram.org/bot{api_key}/sendMessage'
+#
+#    param = {
+#        "chat_id": 332689919,
+#        "text": f'Привет! Время выполнить ПРИВЫЧКУ в МЕСТЕ'
+#    }
+#
+#    print(requests.get(url, param).json())
+#
+#
+#send_telegram_message()
