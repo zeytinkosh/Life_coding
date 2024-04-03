37d6414d360f154ad2f0371c45e9545e8e2bcada
Zeytin <163149798+zeytinkosh@users.noreply.github.com>
2024-04-03 19:09:33 +0300

    Update 2 lambda_train.py

--- lambda_train.py
+++ lambda_train.py
@@ -4,3 +4,11 @@
 numbers = [10, 5, 25, 15, 30, 20]
 maximum = lambda x: max(x)
 print(maximum(numbers))
+
+
+# Напиши код, используя лямбда-выражение, чтобы отфильтровать список и оставить только строки,
+# которые начинаются с заглавной буквы (подсказка: используй метод isupper() для строк)
+
+words = ["Apple", "banana", "Cherry", "Date", "Fig"]
+upper_word = list(filter(lambda word: word.istitle(), words))
+print(upper_word)
